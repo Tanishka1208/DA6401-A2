ef9a47d9bb7893d8a4857b47f30b94bb2fef9384
Tanishka1208 <na22b076@smail.iitm.ac.in>
2026-04-12 01:19:28 +0530

    added the gdown code in multi task

--- models/multitask.py
+++ models/multitask.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-
+import gdown
+import os
 from .vgg11 import VGG11Encoder
 from .layers import CustomDropout
 from .segmentation import VGG11UNet
@@ -9,6 +10,23 @@ from .segmentation import VGG11UNet
 class MultiTaskPerceptionModel(nn.Module):
     def __init__(self, num_breeds=37, seg_classes=3, in_channels=3):
         super().__init__()
+        classifier_path = "checkpoints/classifier.pth"
+        localizer_path = "checkpoints/localizer.pth"
+        unet_path = "checkpoints/unet.pth"
+
+        # Create checkpoints folder if not exists
+        os.makedirs("checkpoints", exist_ok=True)
+
+        # Download weights
+        if not os.path.exists(classifier_path):
+            gdown.download(id="1CcLgl5ppsZJtiPeMv3fhe_JV8rXUM06Y", output=classifier_path, quiet=False)
+
+        if not os.path.exists(localizer_path):
+            gdown.download(id="1Zzla7VFndq9NFJVyagsz8W36doJCBaoi", output=localizer_path, quiet=False)
+
+        if not os.path.exists(unet_path):
+            gdown.download(id="18e7k0Wg9eeoVrQxprPh1q0T_ntM5QA9m", output=unet_path, quiet=False)
+
 
         # ========================
         # SHARED BACKBONE
